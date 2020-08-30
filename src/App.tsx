a68a73d570bbd8885f340424f72bc375811654fe
Marwan Almaseid <MAlmaseid@javna.net>
2020-08-30 12:56:46 +0300

    node-sass & css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import classes from  './App.module.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className={classes.AppHeader}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
